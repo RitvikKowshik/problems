283830767889f1c3264d885ba7a16aaeca4d78d3
RitvikKowshik <ritvikkowshik51@gmail.com>
2024-10-20 00:10:38 +0530

    Add files via upload

---
+++ prog1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Leap{
+    public static void main(String[]args)
+    {
+        int year;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("oi shresth enter year");
+        year=sc.nextInt();
+        if(year%4==0)
+        {
+            System.out.println("It is a leap year");
+        }
+        else{
+            System.out.println("Not leap year");
+        }
+    }
+} 
