70b0cff5efe7f419ae1e4be7d7c98ac1e34c113e
RitvikKowshik <ritvikkowshik51@gmail.com>
2024-10-30 23:19:30 +0530

    Add files via upload

---
+++ Money.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Money {
+    public static void main(String[]args)
+    {
+        double inr,usd;
+        Scanner n=new Scanner(System.in);
+        System.out.println("Enter INR to convert into USD:");
+        inr=n.nextInt();
+        usd=inr/83.33;
+        System.out.println("Rupees="+inr+" is US Dollar="+usd);
+    }
+}
