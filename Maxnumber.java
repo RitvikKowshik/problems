00dd218797d1d24a485acab74a360c18ef30962e
RitvikKowshik <ritvikkowshik51@gmail.com>
2024-11-09 16:06:51 +0530

    Add files via upload

---
+++ Maxnumber.java
@@ -0,0 +1,12 @@
+public class Maxnumber{  
+    public static void main(String[] args) {   
+        int [] arr = new int [] {25, 11, 7, 69, 56};   
+        int max = arr[0];  
+        for (int i = 0; i < arr.length; i++) {    
+           if(arr[i]>max){
+               max = arr[i]; 
+           } 
+        }  
+        System.out.println("Largest number in the array is: " + max);  
+    }  
+}
